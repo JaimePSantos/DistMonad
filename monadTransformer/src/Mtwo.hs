c5252dcac0e82903c1af4bab2a20f42112352e9b
Jaime Santos <50669048+JaimePSantos@users.noreply.github.com>
2020-10-07 23:15:38 +0100

    smaller examples

--- monadTransformer/src/Mtwo.hs
+++ monadTransformer/src/Mtwo.hs
@@ -6,16 +6,29 @@ import Control.Applicative
 import System.IO
 
 
+newtype M2Aux t a = M2Aux {runM2Aux :: (t a, t a)} deriving Show
 
--- data M2 t a = M2 (t (Either a a), t (Either a a))
+newtype EitherAux a = EitherAux (Either a a) deriving Show
+newtype PairAux a = PairAux ((a,a)) deriving Show
+
+
+
+instance Functor EitherAux where
+     fmap f (EitherAux(Right a)) = EitherAux(Right( f a))
+     fmap f (EitherAux(Left a)) = EitherAux(Left( f a)) 
+     
+instance Functor PairAux where
+     fmap f (PairAux(a,b)) =  PairAux(f a, f b)
+
+-- \ \ ---
+
 newtype M2 t a = M2 {runM2 :: (t (Either a a), t (Either a a)) }
-instance (Functor m) => Functor (M2 m) where
-     fmap :: (a -> b) -> (M2 t a -> M2 t b)
-     fmap f (M2 t) = M2 $ (fmap.fmap) f t
-     -- fmap f (M2 t) = M2 $ (fmap.fmap) f' t where
-          -- f' (par) = ?
 
-     -- fmap = undefined
+instance (Functor t) => Functor (M2 t) where
+     fmap :: (a -> b) -> (M2 t a -> M2 t b)
+     -- fmap f (M2 t) = M2 $ (fmap.fmap) f t
+
+     fmap = undefined
 
 instance (Monad t) => Applicative (M2 t) where
      pure :: a -> M2 t a
