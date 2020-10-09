85d9e58cce36c20a102314698102e169ebccf722
Jaime Santos <50669048+JaimePSantos@users.noreply.github.com>
2020-10-09 14:51:07 +0100

    update

--- monadTransformer/src/Mtwo.hs
+++ monadTransformer/src/Mtwo.hs
@@ -31,10 +31,11 @@ import Control.Applicative
 
 --Helper structures
 data Twice a = First a | Second a deriving Show
+data Square a = Pair(a,a) deriving Show
 
-data SquareTwice a = SquareTwice((Twice a, Twice a)) deriving Show
+-- data SquareTwice a = SquareTwice(Square(Twice a, Twice a)) deriving Show
 
-data M2 t a = M2 ( SquareTwice( t a)) deriving Show
+data M2 t a = M2 (Square(t Twice a)) --deriving Show
 
 
 --Twice
@@ -56,59 +57,83 @@ instance Monad Twice where
      return = pure
      (>>=) = undefined
 
---SquareTwice   
-instance Functor SquareTwice where
-     fmap f (SquareTwice(First a,First b)) = SquareTwice (First $ f a,  First $ f b)
-     fmap f (SquareTwice(First a,Second b)) =SquareTwice  (First $ f a,  Second $ f b)
-     fmap f (SquareTwice(Second a,First b)) = SquareTwice (Second $ f a,  First $ f b)
-     fmap f (SquareTwice(Second a,Second b)) =SquareTwice (Second $ f a,  Second $ f b)
+--Square
 
-instance Applicative SquareTwice where
-     pure = return
-     (SquareTwice(First f,First g)) <*> (SquareTwice(First a,First b)) = SquareTwice (First $ f a,  First $ g b)
-     (SquareTwice(First f,Second g)) <*> (SquareTwice(First a,Second b)) = SquareTwice (First $ f a,  Second $ g b)
-     (SquareTwice(Second f,First g)) <*> (SquareTwice(Second a,First b)) = SquareTwice (Second $ f a,  First $ g b)
-     (SquareTwice(Second f,Second g)) <*> (SquareTwice(Second a,Second b)) = SquareTwice (Second $ f a,  Second $ g b)
+instance Functor Square where
+     fmap f (Pair(a,b) )= Pair(f a, f b)
 
-     (SquareTwice(First f,First g)) <*> _ = undefined
-     (SquareTwice(First f,Second g)) <*> _ = undefined
-     (SquareTwice(Second f,First g)) <*> _ = undefined
-     (SquareTwice(Second f,Second g)) <*> _ = undefined
+instance Applicative Square where
+     Pair(f,g) <*> Pair(a,b) = Pair(f a, g b)
 
-instance Monad SquareTwice where
+instance Monad Square where
      return = pure
      (>>=) = undefined
 
-
--- \\ -- \\ -- \\ -- \\ --
 --M2
-instance (Functor t) => Functor(M2 t ) where
+instance (Functor t) => Functor(M2 t) where
      fmap :: (a -> b) -> (M2 t a -> M2 t b)
      fmap f (M2 t) = M2 $ (fmap.fmap) f t
 
-instance (Monad t) => Applicative (M2 t) where
-     pure :: a -> M2 t a
-     pure = M2 . pure .pure
-     M2 f <*> M2 t = M2 $ (<*>) <$> f <*> t
-     -- (<*>) = ap
+--M2
+-- instance Functor M2 where
+--      fmap = undefined
 
-instance (Monad t) => Monad (M2 t) where
-     return :: a -> M2 t a
-     return = pure
-     (>>=) :: M2 t a -> (a -> M2 t b) -> M2 t b
-     (>>=)= undefined
+-- instance Applicative M2 where
+--      pure = return
+--      (<*>) = ap
+
+--SquareTwice   
+-- instance Functor SquareTwice where
+--      fmap f (SquareTwice(First a,First b)) = SquareTwice (First $ f a,  First $ f b)
+--      fmap f (SquareTwice(First a,Second b)) =SquareTwice  (First $ f a,  Second $ f b)
+--      fmap f (SquareTwice(Second a,First b)) = SquareTwice (Second $ f a,  First $ f b)
+--      fmap f (SquareTwice(Second a,Second b)) =SquareTwice (Second $ f a,  Second $ f b)
+
+-- instance Applicative SquareTwice where
+--      pure = return
+--      (SquareTwice(First f,First g)) <*> (SquareTwice(First a,First b)) = SquareTwice (First $ f a,  First $ g b)
+--      (SquareTwice(First f,Second g)) <*> (SquareTwice(First a,Second b)) = SquareTwice (First $ f a,  Second $ g b)
+--      (SquareTwice(Second f,First g)) <*> (SquareTwice(Second a,First b)) = SquareTwice (Second $ f a,  First $ g b)
+--      (SquareTwice(Second f,Second g)) <*> (SquareTwice(Second a,Second b)) = SquareTwice (Second $ f a,  Second $ g b)
+
+--      (SquareTwice(First f,First g)) <*> _ = undefined
+--      (SquareTwice(First f,Second g)) <*> _ = undefined
+--      (SquareTwice(Second f,First g)) <*> _ = undefined
+--      (SquareTwice(Second f,Second g)) <*> _ = undefined
+
+-- instance Monad SquareTwice where
+--      return = pure
+--      (>>=) = undefined
+
+
+-- \\ -- \\ -- \\ -- \\ --
+
+-- instance (Monad t) => Applicative (M2 t) where
+--      pure :: a -> M2 t a
+--      pure = M2 . pure .pure
+--      M2 f <*> M2 t = M2 $ (<*>) <$> f <*> t
+--      (<*>) = ap
+
+-- instance (Monad t) => Monad (M2 t) where
+--      return :: a -> M2 t a
+--      return = pure
+--      (>>=) :: M2 t a -> (a -> M2 t b) -> M2 t b
+--      (>>=)= undefined
 
 
 -- \\ -- \\ -- \\ -- \\ --
 --Examples
 
-a = M2 $ SquareTwice (First [1],Second [2])
-b = M2 $ SquareTwice (First (Just 1),Second Nothing)
-c = M2 $ SquareTwice (First [1],Second [2])
+a = M2 $ Square([First 1], [First 2])
+-- b = M2 $ SquareTwice (First (Just 1),Second Nothing)
+-- c = M2 $ SquareTwice (First [1],Second [2])
 
-plusTwo = (+2) 
-plusTwo' = (+2) 
+-- plusTwo = (+2) 
+-- plusTwo' = (+2) 
 
-aux = M2 $(SquareTwice(First[(+2),(+2)],First[(+2)]))
+-- aux = M2 $(SquareTwice(First[(+2),(+2)],First[(+2)]))
 
 plusTwo'' = aux <*> a 
+
+-- (f <<= ) = f#
+-- x >>= f = f# x
