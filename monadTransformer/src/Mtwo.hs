be88bf8b00aa66e8abb758bd8f94dfba1c2b403c
Jaime Santos <50669048+JaimePSantos@users.noreply.github.com>
2020-10-09 13:54:07 +0100

    squaretwice applicative

--- monadTransformer/src/Mtwo.hs
+++ monadTransformer/src/Mtwo.hs
@@ -29,12 +29,15 @@ import Control.Applicative
 --      fmap f (SquareTwice(Square(Second a,First b))) = SquareTwice $ Square (Second $ f a,  First $ f b)
 --      fmap f (SquareTwice(Square(Second a,Second b))) =SquareTwice $ Square (Second $ f a,  Second $ f b)
 
+--Helper structures
 data Twice a = First a | Second a deriving Show
 
 data SquareTwice a = SquareTwice((Twice a, Twice a)) deriving Show
 
 data M2 t a = M2 ( SquareTwice( t a)) deriving Show
 
+
+--Twice
 instance Functor Twice where
      fmap f (First a) = First( f a)
      fmap f (Second a) = Second( f a) 
@@ -43,6 +46,8 @@ instance Applicative Twice where
      pure a = First a
      First f <*> First a = First(f a)
      Second f <*> Second a = Second(f a) 
+     Second f <*> First a = undefined
+     First f <*> Second a = undefined
      --duvida em como emparelhar elementos diferentes do tipo
      -- Second f <*> First a = First(f a)
      -- First f <*> Second a = Second(f a)
@@ -50,7 +55,8 @@ instance Applicative Twice where
 instance Monad Twice where
      return = pure
      (>>=) = undefined
-     
+
+--SquareTwice   
 instance Functor SquareTwice where
      fmap f (SquareTwice(First a,First b)) = SquareTwice (First $ f a,  First $ f b)
      fmap f (SquareTwice(First a,Second b)) =SquareTwice  (First $ f a,  Second $ f b)
@@ -59,14 +65,23 @@ instance Functor SquareTwice where
 
 instance Applicative SquareTwice where
      pure = return
-     (<*>) = undefined
+     (SquareTwice(First f,First g)) <*> (SquareTwice(First a,First b)) = SquareTwice (First $ f a,  First $ g b)
+     (SquareTwice(First f,Second g)) <*> (SquareTwice(First a,Second b)) = SquareTwice (First $ f a,  Second $ g b)
+     (SquareTwice(Second f,First g)) <*> (SquareTwice(Second a,First b)) = SquareTwice (Second $ f a,  First $ g b)
+     (SquareTwice(Second f,Second g)) <*> (SquareTwice(Second a,Second b)) = SquareTwice (Second $ f a,  Second $ g b)
+
+     (SquareTwice(First f,First g)) <*> _ = undefined
+     (SquareTwice(First f,Second g)) <*> _ = undefined
+     (SquareTwice(Second f,First g)) <*> _ = undefined
+     (SquareTwice(Second f,Second g)) <*> _ = undefined
 
 instance Monad SquareTwice where
      return = pure
      (>>=) = undefined
 
--- \\ -- \\ -- \\ -- \\ --
 
+-- \\ -- \\ -- \\ -- \\ --
+--M2
 instance (Functor t) => Functor(M2 t ) where
      fmap :: (a -> b) -> (M2 t a -> M2 t b)
      fmap f (M2 t) = M2 $ (fmap.fmap) f t
@@ -83,8 +98,9 @@ instance (Monad t) => Monad (M2 t) where
      (>>=) :: M2 t a -> (a -> M2 t b) -> M2 t b
      (>>=)= undefined
 
--- \\ -- \\ -- \\ -- \\ --
 
+-- \\ -- \\ -- \\ -- \\ --
+--Examples
 
 a = M2 $ SquareTwice (First [1],Second [2])
 b = M2 $ SquareTwice (First (Just 1),Second Nothing)
@@ -93,6 +109,6 @@ c = M2 $ SquareTwice (First [1],Second [2])
 plusTwo = (+2) 
 plusTwo' = (+2) 
 
-aux = M2 $(SquareTwice(First[(+2)],Second[(+2)]))
+aux = M2 $(SquareTwice(First[(+2),(+2)],First[(+2)]))
 
-plusTwo'' = aux <*> a -- esta a dar erro indefinido :(
+plusTwo'' = aux <*> a 
