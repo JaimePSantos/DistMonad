d88d804994affdfeb40cae1cc4e6df0c588a79ad
jaimepsantos <jaimepereirasantos123@gmail.com>
2021-02-01 22:23:30 +0000

    Polishing Set monad instance.

--- jacobsWalk/src/Set.hs
+++ jacobsWalk/src/Set.hs
@@ -12,14 +12,49 @@ module Set where
 
 import qualified Data.Set as S
 import Control.Monad       (liftM, ap,mapM)
+import qualified Data.Foldable as Foldable
+
+-- First approach is to wrap most of the Set structure into a new one and instance that as a monad in order to use it elsewhere. Perhaps this is pointless.
+-- What would happen if we instance Data.Set as a monad in this file, and try to use it's monadic properties in QWalk, for example?
+newtype SetMon a = SetMon{unSetMon :: S.Set a}
+
+instance (Show a) => Show(SetMon a) where
+  show(SetMon x) = show(x)
+
+-- Migrating Set functions to SetMon.
+-- Construction
+empty  :: SetMon a
+empty = SetMon $ S.empty
+singleton :: a -> SetMon a
+singleton a = SetMon $ S.singleton (a)
+fromList :: Ord a => [a] -> SetMon a
+fromList a = SetMon $ S.fromList a
+
+--Insertion
+insert :: Ord a => a -> SetMon a -> SetMon a
+insert a s = SetMon $ S.insert a (unSetMon s)
+
+--Deletion
+delete :: Ord a => a -> SetMon a -> SetMon a
+delete a s = SetMon $ S.delete a (unSetMon s)
+
+--Combine
+union :: Ord a => SetMon a -> SetMon a -> SetMon a
+union s s' = SetMon $ S.union (unSetMon s) (unSetMon s')
+unions :: (Foldable f, Ord a) => f (SetMon a) -> SetMon a
+unions = Foldable.foldl' union empty
+difference :: Ord a => SetMon a -> SetMon a -> SetMon a
+difference s s' = SetMon $ S.difference (unSetMon s) (unSetMon s')
+intersection :: Ord a => SetMon a -> SetMon a -> SetMon a
+intersection s s' = SetMon $ S.intersection (unSetMon s) (unSetMon s')
 
 class OrdMonad m where
    ordReturn :: Ord a => a -> m a
    ordBind :: (Ord a, Ord b) => m a -> (a -> m b) -> m b
 
-instance OrdMonad S.Set where
-   ordReturn  = S.singleton
-   s `ordBind`  f = S.fold (\v ret -> f v `S.union` ret) S.empty s
+instance OrdMonad SetMon where
+   ordReturn  = SetMon. S.singleton
+   s `ordBind`  f =  SetMon$ (S.fold (\v ret -> unSetMon(f v) `S.union` ret) S.empty (unSetMon(s)))
 
 data AsMonad m a where
   Embed :: (OrdMonad m, Ord a) => m a -> AsMonad m a
@@ -45,7 +80,7 @@ unEmbed (Bind (Embed m) f) = m `ordBind` (unEmbed . f)
 unEmbed (Bind (Return v) f) = unEmbed (f v)
 unEmbed (Bind (Bind m f) g) = unEmbed (Bind m (\x -> Bind (f x) g))
 
-setExample = unEmbed $ do
-  x <- Embed $ S.fromList [1,2,4]
-  y <- Embed $ S.fromList [1,2,4]
-  return (x*y)
+--setExample = unEmbed $ do
+--  x <- Embed $ S.fromList [1,2,4]
+--  y <- Embed $ S.fromList [1,2,4]
+--  return (x*y)
--- jacobsWalk/src/SetExample.hs
+++ jacobsWalk/src/SetExample.hs
@@ -6,7 +6,9 @@
 module SetExample where
 
 import qualified Data.Set as S
+import qualified Set as SM 
 
+--Data.Set examples
 exampleSet1 = S.fromList [1,2,3]
 exampleSet2 = S.fromList [4,5,6]
 exampleUnion = S.union exampleSet1 exampleSet2
@@ -23,3 +25,21 @@ exampleEmpty = S.empty
 
 exampleDelete = S.delete 3 exampleSet2
 exampleInsert = S.insert 7 exampleSet2
+
+--SetMonad Examples
+exampleSM1 = SM.fromList[1,2,3]
+exampleSM2 = SM.fromList [4,5,6]
+exampleSMUnion = SM.union exampleSM1 exampleSM2
+exampleSMDifference = SM.difference (SM.insert 5 exampleSM1) exampleSM2
+exampleSMIntersection = SM.intersection (SM.insert 5 exampleSM1) (SM.insert 3 exampleSM2)
+
+exampleSMList :: [SM.SetMon Int]
+exampleSMList = [exampleSM1, exampleSM2]
+exampleSMListUnion = SM.unions exampleSMList
+
+exampleSMSingleton :: SM.SetMon Int
+exampleSMSingleton = SM.singleton 1
+exampleSMEmpty = SM.empty
+
+exampleSMDelete = SM.delete 3 exampleSM2
+exampleSMInsert = SM.insert 7 exampleSM2
